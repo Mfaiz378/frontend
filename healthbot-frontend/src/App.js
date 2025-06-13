e44616db9846722d89e81f6d90f5a7bd4be0aa1b
Mfaiz378 <105097686+Mfaiz378@users.noreply.github.com>
2025-06-13 22:52:43 +0530

    Update App.js

--- healthbot-frontend/src/App.js
+++ healthbot-frontend/src/App.js
@@ -9,32 +9,36 @@ import HospitalView from './pages/HospitalView';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={
-        <ProtectedRoute>
-          <Home />
-        </ProtectedRoute>
-      } />
-      <Route path="/chatbot" element={
-        <ProtectedRoute>
-          <Chatbot />
-        </ProtectedRoute>
-      } />
-      <Route path="/map" element={
-        <ProtectedRoute>
-          <MapView />
-        </ProtectedRoute>
-      } />
-      <Route path="/hospitals" element={
-        <ProtectedRoute>
-          <HospitalView />
-        </ProtectedRoute>
-      } />
-    </Routes>
-  );
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/chatbot" element={
+        <ProtectedRoute>
+          <Chatbot />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/map" element={
+        <ProtectedRoute>
+          <MapView />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/hospitals" element={
+        <ProtectedRoute>
+          <HospitalView />
+        </ProtectedRoute>
+      } />
+    </Routes>
+  );
 };
 
 export default App;
